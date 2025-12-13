d50bd935c22b4654b46d503d0b7208d5acab84a1
hanfeibo <1607670708@qq.com>
2025-12-13 17:32:25 +0800

    需求：{第9节：接入openai}

--- hfb-dev-tech-api/src/main/java/com/hanfb/dev/tech/api/IAiService.java
+++ hfb-dev-tech-api/src/main/java/com/hanfb/dev/tech/api/IAiService.java
@@ -1,6 +1,9 @@
 package com.hanfb.dev.tech.api;
 
 import org.springframework.ai.chat.ChatResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Flux;
 
 public interface IAiService {
@@ -9,4 +12,6 @@ public interface IAiService {
 
     Flux<ChatResponse> generateStream(String model, String message);
 
+    @RequestMapping(value = "generate_stream_rag", method = RequestMethod.GET)
+    Flux<ChatResponse> generateStreamRag(@RequestParam String model, @RequestParam String ragTag, @RequestParam String message);
 }
